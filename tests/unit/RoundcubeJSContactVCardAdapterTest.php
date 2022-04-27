1f6066df9d789ce943c429c7144682fd550f51b1
Stanimir Bozhilov <stanimir@audriga.com>
2022-04-27 16:28:45 +0200

    Add first unit test for JSContact - vCard conversion for RC

---
+++ tests/unit/RoundcubeJSContactVCardAdapterTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace OpenXPort\Test\VCard;
+
+use OpenXPort\Adapter\RoundcubeJSContactVCardAdapter;
+use OpenXPort\Jmap\JSContact\Audriga\Card;
+use OpenXPort\Jmap\JSContact\Phone;
+use OpenXPort\Mapper\RoundcubeJSContactVCardMapper;
+use PHPUnit\Framework\TestCase;
+use Sabre\VObject\Reader;
+
+final class RoundcubeJSContactVCardAdapterTest extends TestCase
+{
+    /** @var \Sabre\VObject\Component\VCard */
+    protected $vCard = null;
+
+    /** @var \OpenXPort\Adapter\RoundcubeJSContactVCardAdapter */
+    protected $adapter = null;
+
+    /** @var \OpenXPort\Mapper\RoundcubeJSContactVCardMapper */
+    protected $mapper = null;
+
+    /** @var array */
+    protected $vCardData = null;
+
+    /** @var \OpenXPort\Jmap\JSContact\Card */
+    protected $jsContactCard = null;
+
+    public function setUp(): void
+    {
+        $this->adapter = new RoundcubeJSContactVCardAdapter();
+        $this->mapper = new RoundcubeJSContactVCardMapper();
+    }
+
+    public function tearDown(): void
+    {
+        $this->vCard = null;
+        $this->adapter = null;
+        $this->mapper = null;
+        $this->vCardData = null;
+        $this->jsContactCard = null;
+    }
+
+    /**
+     * This test aims to check that in the case of Roundcube the value 'other' is not added to 'contexts'
+     * of 'phones' entries when 'contexts' is null.
+     * It also checks that the 'features' property remains intact for entries of 'phones'.
+     */
+    public function testCorrectRoundcubeRoundtripPhones()
+    {
+        $this->jsContactCard = new Card();
+        $pagerPhoneEntry = new Phone();
+        $pagerPhoneEntry->setAtType("Phone");
+        $pagerPhoneEntry->setPhone("123-pager");
+        $pagerPhoneEntry->setFeatures(["pager" => true]);
+
+        $pagerOtherPhoneEntry = new Phone();
+        $pagerOtherPhoneEntry->setAtType("Phone");
+        $pagerOtherPhoneEntry->setPhone("123-pager-other");
+        $pagerOtherPhoneEntry->setFeatures(["pager" => true]);
+        $pagerOtherPhoneEntry->setContexts(["other" => true]);
+
+        $this->jsContactCard->setPhones([
+            "123-pager" => $pagerPhoneEntry,
+            "123-pager-other" => $pagerOtherPhoneEntry
+        ]);
+
+        $jsContactData = array("c1" => json_decode(json_encode($this->jsContactCard)));
+
+        $this->vCardData = $this->mapper->mapFromJmap($jsContactData, $this->adapter);
+
+        $resultingJsContactCard = $this->mapper->mapToJmap(reset($this->vCardData), $this->adapter)[0];
+
+        $this->assertEquals(
+            array_values($this->jsContactCard->getPhones()),
+            array_values($resultingJsContactCard->getPhones())
+        );
+    }
+}
